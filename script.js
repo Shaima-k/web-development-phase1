7e0b81d4b75e9a934587e68b640877ad36041585
Shaima <sh8880@hotmail.com>
2021-07-19 03:40:36 +0300

    project 1

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function focusevent()
+	    {
+		    document.getElementById("input1").style.background="#17A2B8";
+	    }
+function clickevent(){
+  document.getElementById("input-h").style.color="#17A2B8";
+}
