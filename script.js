55bd917488019afbcba9aefb2b87e046e4d896b2
Shaima <sh8880@hotmail.com>
2021-08-02 18:02:31 +0300

    added post section :project1

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-
-function focusevent()
-	    {
-		    document.getElementById("input1").style.background="#17A2B8";
-	    }
-function clickevent(){
-  document.getElementById("input-h").style.color="#17A2B8";
+function focusevent() {
+    document.getElementById("input1").style.background = "#17A2B8";
 }
+
+function clickevent() {
+    document.getElementById("input-h").style.color = "#17A2B8";
+}
+ClassicEditor
+    .create(document.querySelector('#body'), {
+        toolbar: ['bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', 'uploadImage'],
+        heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+            ]
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
